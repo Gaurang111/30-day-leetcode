49fa6748f77392732b9b6b0003c5bb9ba0179f38
Gaurang Solanki <58483854+Gaurang111@users.noreply.github.com>
2024-04-24 16:38:03 -0500

    Add files via upload

---
+++ 30days leetcode/Problem310.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def findMinHeightTrees(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        if n == 1:
+            return [0]
+
+        adj_list = {i: set() for i in range(n)}
+        for u, v in edges:
+            adj_list[u].add(v)
+            adj_list[v].add(u)
+
+        leaves = [i for i in range(n) if len(adj_list[i]) == 1]
+
+        while n > 2:
+
+            n -= len(leaves)
+            new_leaves = []
+            for leaf in leaves:
+                neighbor = adj_list[leaf].pop()
+                adj_list[neighbor].remove(leaf)
+
+                if len(adj_list[neighbor]) == 1:
+                    new_leaves.append(neighbor)
+            leaves = new_leaves
+
+        return leaves
