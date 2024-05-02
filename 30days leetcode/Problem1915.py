05f8e5a27a6adfc2423364a5696ca9b71f591d30
Gaurang Solanki <58483854+Gaurang111@users.noreply.github.com>
2024-05-02 17:54:25 -0500

    Add files via upload

---
+++ 30days leetcode/Problem1915.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def wonderfulSubstrings(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        count = [0] * 1024
+        count[0] = 1
+        bitmask = 0
+        result = 0
+
+        for char in word:
+            char_index = ord(char) - ord('a')
+            bitmask ^= 1 << char_index
+            result += count[bitmask]
+            for i in range(10):
+                result += count[bitmask ^ (1 << i)]
+            count[bitmask] += 1
+
+        return result
